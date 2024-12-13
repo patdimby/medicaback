ef1c4c8c69483626822b7d02413c7e39aad9dfda
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2024-12-13 22:24:01 +0300

    go
    go

--- Medica.Core/Domains/TodoItem.cs
+++ Medica.Core/Domains/TodoItem.cs
@@ -10,10 +10,18 @@ public class TodoItem : AuditEntity<Guid>
     [Column("due")]
     [JsonProperty(PropertyName = "due_date")]
     public DateTime? DueDate { get; set; }
-    [NotMapped]
-    public List<string> Labels { get; set; } = [];
+
+    [NotMapped] public List<string> Labels { get; set; } = [];
+
     [Column("is_completed")]
     [JsonProperty(PropertyName = "is_completed")]
     public bool IsCompleted { get; set; }
-    
+
+    [Column("title")]
+    [JsonProperty(PropertyName = "title")]
+    public string Title { get; set; } = "";
+
+    [Column("assignee")]
+    [JsonProperty(PropertyName = "assignee")]
+    public string Assignee { get; set; } = "";
 }
